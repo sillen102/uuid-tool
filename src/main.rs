83453be81d8a394d14166a61eff4a7fde77f1ac0
Silvio Sabo <silvio.sabo@waya.se>
2022-12-26 18:04:13 +0100

    Added argument parsing and ability to generate multiple uuids

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use clap::Parser;
 use uuid::Uuid;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Number of uuids to generate
+    #[arg(short, long, default_value_t = 1)]
+    number: u8,
+}
+
 fn main() {
-    let id = Uuid::new_v4();
-    println!("{}", id);
+    let args = Args::parse();
+    for _ in 0..args.number {
+        println!("{}", Uuid::new_v4());
+    }
 }
